fce4d26baf50b74f94f2043f4a7c343ca3b55c67
Marie_Tignol <marie-tignol_student2022@wilder.school>
2023-04-28 16:56:41 +0200

    division of the test in several parts

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,21 +1,50 @@
 describe('Success', () => {
-  it('passes', () => {
+  it('Scénario 1 : Accéder à la page Tops ', () => {
     cy.visit('https://magento.softwaretestingboard.com/')
-
-    /// debut scenario 1 à 5 ////
-
     cy.get('#ui-id-4').trigger('mouseover')
     cy.get('#ui-id-9').click()
-    cy.get('.logo > img').click()
+  })
+  it('Scénario 1 bis : Accéder à la page Tops ', () => {
+    cy.visit('https://magento.softwaretestingboard.com/')
     cy.get('#ui-id-4').click()
     cy.get('dd > .items > :nth-child(1) > a').click()
+  })
+  it('Scénario 2 : visualiser  un produit', () => {
+    cy.visit('https://magento.softwaretestingboard.com/women/tops-women.html')
     cy.get('.wrapper > .products > :nth-child(1)').click()
+  })
+  it('Scénario 3 : Sélectionner une taille', () => {
+    cy.visit('https://magento.softwaretestingboard.com/breathe-easy-tank.html')
+    cy.get('#option-label-size-143-item-168').trigger('mouseover').click()
+  })
+  it('Scénario 4 : Sélectionner une couleur', () => {
+    cy.visit('https://magento.softwaretestingboard.com/breathe-easy-tank.html')
+    cy.get('#option-label-size-143-item-168').trigger('mouseover').click()
+    cy.get('#option-label-color-93-item-59').click()
+  })
+  it('Scénario 5 : Ajout dn produit dans le panier', () => {
+    cy.visit('https://magento.softwaretestingboard.com/breathe-easy-tank.html')
     cy.get('#option-label-size-143-item-168').trigger('mouseover').click()
     cy.get('#option-label-color-93-item-59').click()
     cy.get('#product-addtocart-button').click()
     cy.get('.showcart').should('be.visible')
-    cy.get('.message-success > div').should('be.visible')
   })
 })
 
-/// fin scenario 1 à 5 ////
+/**
+ *  SANS It
+ *  cy.visit('https://magento.softwaretestingboard.com/')
+    cy.get('#ui-id-4').trigger('mouseover')
+    cy.get('#ui-id-9').click()
+    cy.get('#ui-id-4').click()
+    cy.get('dd > .items > :nth-child(1) > a').click()
+    cy.get('.wrapper > .products > :nth-child(1)').click()
+    cy.get('#option-label-size-143-item-168').trigger('mouseover').click()
+    cy.get('#option-label-size-143-item-168').trigger('mouseover').click()
+    cy.get('#option-label-color-93-item-59').click()
+    cy.get('#option-label-size-143-item-168').trigger('mouseover').click()
+    cy.get('#option-label-color-93-item-59').click()
+    cy.get('#product-addtocart-button').click()
+    cy.get('.showcart').should('be.visible')
+*/
+
