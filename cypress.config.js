81403e7b3f274d301cda3bb4c1af4311fcbcfbba
mac1 <mac1@lazf.home>
2023-05-12 09:19:45 +0200

    add code for rapport html

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,10 @@
-module.exports = {
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
-  video: true,
-};
+});
